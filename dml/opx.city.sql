4278cd6492f6be10037a1abdb699c6bb6a7431f2
JuanManu59 <33491290+JuanManu59@users.noreply.github.com>
2020-12-17 17:10:38 -0500

    Información pre cargada en el sistema

---
+++ dml/opx.city.sql
@@ -0,0 +1,1 @@
+INSERT INTO opx.city(city_id, city_name) VALUES('76001', 'Santiago de Cali');
---
+++ dml/opx.project_type.sql
@@ -0,0 +1,2 @@
+INSERT INTO opx.project_type(projtype_id, projtype_name, projtype_description) VALUES('0c101327-e085-4e87-972a-e06999b175b5','Medición','medición');
+INSERT INTO opx.project_type(projtype_id, projtype_name, projtype_description) VALUES('dfec2a2f-7ad5-4b15-bb71-950bd248d346','Impacto','impacto');
