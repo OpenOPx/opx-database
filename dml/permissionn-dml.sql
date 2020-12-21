4aafadae945191f379510ca009b9b033808cb16b
leofrancocalpa <francocalpa@gmail.com>
2020-12-21 18:00:30 -0500

    others dml files added

--- dml/permissionn-dml.sql
+++ dml/permissionn-dml.sql
@@ -1,1 +1,9 @@
 -- permissionn data
+INSERT INTO opx.permissionn(perm_id, perm_codename, perm_name) VALUES('f961def1-ab3a-48ad-90e2-caa4be807edf', 'view_project', 'Ver Proyectos')
+INSERT INTO opx.permissionn(perm_id, perm_codename, perm_name) VALUES('96ea710f-af37-42cb-8634-cbfb7bcc9866', 'add_project', 'Crear Proyectos')
+INSERT INTO opx.permissionn(perm_id, perm_codename, perm_name) VALUES('36817938-3e88-4c69-8fcd-1a2937f0f3f1', 'change_project', 'Editar Proyectos')
+INSERT INTO opx.permissionn(perm_id, perm_codename, perm_name) VALUES('32c43d05-a424-4b3e-ab86-05abb00912b0', 'delete_project', 'Eliminar Proyectos')
+INSERT INTO opx.permissionn(perm_id, perm_codename, perm_name) VALUES('', 'view_', 'Ver ')
+INSERT INTO opx.permissionn(perm_id, perm_codename, perm_name) VALUES('', 'add_', 'Crear ')
+INSERT INTO opx.permissionn(perm_id, perm_codename, perm_name) VALUES('', 'change_', 'Editar ')
+INSERT INTO opx.permissionn(perm_id, perm_codename, perm_name) VALUES('', 'delete_', 'Eliminar ')
---
+++ dml/role-dml.sql
@@ -0,0 +1,5 @@
+-- role
+INSERT INTO opx.role(role_id, role_name, role_description, isactive) VALUES('8945979e-8ca5-481e-92a2-219dd42ae9fc', 'Super Administrador', 'Puede acceder a todos los recursos del sistema', 1)
+INSERT INTO opx.role(role_id, role_name, role_description, isactive) VALUES('628acd70-f86f-4449-af06-ab36144d9d6a', 'Proyectista', 'Puede crear y gestionar proyectos y equipos ', 1)
+INSERT INTO opx.role(role_id, role_name, role_description, isactive) VALUES('53ad3141-56bb-4ee2-adcf-5664ba03ad65', 'Validador', 'Puede realizar las tareas de los proyectos activos', 1)
+INSERT INTO opx.role(role_id, role_name, role_description, isactive) VALUES('0be58d4e-6735-481a-8740-739a73c3be86', 'Voluntario', 'Puede realizar y validar las tareas de los proyectos activos ', 1)
