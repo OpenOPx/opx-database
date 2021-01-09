c9412cd92307033d80a189d421f335d7fe6aae79
leofrancocalpa <francocalpa@gmail.com>
2021-01-09 18:06:57 -0500

    database scripts v1.0

--- dml/dml-individual-files/opx_dimension_type-dml.sql
+++ dml/dml-individual-files/opx_dimension_type-dml.sql
@@ -2,6 +2,3 @@
 INSERT INTO opx.dimension_type(dim_type_id, dim_type_name, dim_type_description) VALUES('35b0b478-9675-45fe-8da5-02ea9ef88f1b', 'Urbana', 'Grandes ciudades industrializadas.');
 INSERT INTO opx.dimension_type(dim_type_id, dim_type_name, dim_type_description) VALUES('3934414c-c15e-4ddc-bdee-9c434237caa7', 'Rural', 'Grandes campos donde predominan las actividades agropecuarias.');
 
-f283a6fa-6ba5-477a-af64-c0507cd48c2d
-
-ba46b9d5-2c92-44e2-94f4-4eea64b39eba -- cali hurtos
--- dml/opx.dimension_type-dml.sql
+++
@@ -1,3 +0,0 @@
--- dimension_type
-INSERT INTO opx.dimension_type(dim_type_id, dim_type_name, dim_type_description) VALUES('35b0b478-9675-45fe-8da5-02ea9ef88f1b', 'Urbana', 'Grandes ciudades industrializadas.');
-INSERT INTO opx.dimension_type(dim_type_id, dim_type_name, dim_type_description) VALUES('3934414c-c15e-4ddc-bdee-9c434237caa7', 'Rural', 'Grandes campos donde predominan las actividades agropecuarias.');
